719882478620af158da702ea97de684580791ca4
OpenSpace <mensindex@gmail.com>
2022-03-18 14:23:49 +0300

    domain layer is finished

---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,15 @@
+package com.example.shoppinglist.domain
+
+interface ShopListRepository {
+
+    fun addShopItem(item: ShopItem)
+
+    fun deleteShopItem(shopItem: ShopItem)
+
+    fun editShopItem(shopItem: ShopItem)
+
+    fun getShopItem(id: Int): ShopItem
+
+    fun getShopList(): List<ShopItem>
+
+}
