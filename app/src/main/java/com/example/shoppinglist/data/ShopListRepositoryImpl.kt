cab3a8c5306b809a445f0b465267425fd352be6b
OpenSpace <mensindex@gmail.com>
2022-03-21 09:51:37 +0300

    implementation by linear layout

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -9,13 +9,13 @@ import java.lang.RuntimeException
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLiveData = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
     private var autoIncrementId = 0
 
     init {
         for (i in 0 until 10) {
             val item = ShopItem("Name $i", i, true)
-            shopList.add(item)
+            addShopItem(item)
         }
     }
 
@@ -45,7 +45,6 @@ object ShopListRepositoryImpl : ShopListRepository {
     }
 
     override fun getShopList(): LiveData<List<ShopItem>> {
-        updateList()
         return shopListLiveData
     }
 
